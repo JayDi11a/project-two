0ab049b7cb7f7dc96679e90e945465570ebba464
Jaydi11a <geraldtrotman@fas.harvard.edu>
2014-07-07 19:49:38 -0400

    adding to index file. Currently working on adding check boxes and buttons and text fields.

--- index.php
+++ index.php
@@ -2,3 +2,16 @@
 error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
 ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Password Generator</title>
+
+	<?php require 'logic.php'; ?>
+
+</head>
+<body>
+
+	<form method='POST' action='index.php'>
